70791e4bd9a7533225684a13fb842d05848d0c70
Sameer Anand <56514538+sameeranand12@users.noreply.github.com>
2020-10-01 00:27:56 +0530

    Create sort.c

---
+++ sort.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main() {
+    int n; 
+    scanf("%d", &n);
+    int *a = malloc(sizeof(int) * n);
+    for(int a_i = 0; a_i < n; a_i++){
+    	scanf("%d",&a[a_i]);
+    
+    return 0;
+}
