3d0139a7f0e296c1b577825b3701b39839c80712
4bh1n4v <54211410+4bh1n4v@users.noreply.github.com>
2020-10-01 01:53:00 +0530

    adding integers
    may help someone to add integers

---
+++ Ide.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+import java.util.Scanner;
+class AddNumbers
+{
+   public static void main(String args[])
+   {
+      int x, y, z;
+
+      System.out.println("Enter two integers to calculate their sum");
+      Scanner in = new Scanner(System.in);
+     
+      x = in.nextInt();
+      y = in.nextInt();
+      z = x + y;
+     
+      System.out.println("Sum of the integers = " + z);
+   }
+}
