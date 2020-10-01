b8a2ff95fc8bfc2828ff71dea92bc5e5a2b7d045
Vishal <201951171@iiitvadodara.ac.in>
2020-10-01 09:46:08 +0530

    code made more readable

--- Ide (1).java
+++ Ide (1).java
@@ -1,19 +1,19 @@
 /* package codechef; // don't place package name! */
 
 import java.util.Scanner;
-class subtractNumbers
+class subtractNumbers // class statement 
 {
-   public static void main(String args[])
+   public static void main(String args[]) // main from where program will start to wrok
    {
       int x, y, z;
 
       System.out.println("Enter two integers to calculate their difference");
-      Scanner in = new Scanner(System.in);
+      Scanner in = new Scanner(System.in); // creating sacanner object to input output operations
      
       x = in.nextInt();
       y = in.nextInt();
       z = x - y;
      
-      System.out.println("difference of the integers = " + z);
+      System.out.println("difference of the integers = " + z); // printing results
    }
 }
