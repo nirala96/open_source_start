08c20fce12beeeae770c74b98f46d87c7d1fe1bb
K.L.Bunkar <73030996+kantibunkar@users.noreply.github.com>
2020-10-17 22:15:32 +0530

    Add files via upload

---
+++ Prog7 (DivideUN).cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include<iostream>
+int main()
+{
+	//Let's Divide the two numbers
+	int a,b;
+	cout <<"Enter The Value of A\n";
+	cin >>a;
+	cout <<"Enter The Value of B\n";
+	cin >>b;
+	
+	cout <<"Divide" " " << a/b;;
+	return 0;
+}
