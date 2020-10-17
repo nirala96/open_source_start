69a56b77875ec0d1a72e7bcd70d896d5713b6590
K.L.Bunkar <73030996+kantibunkar@users.noreply.github.com>
2020-10-17 21:57:29 +0530

    Add files via upload

---
+++ Prog8  (Calculator).cpp
@@ -0,0 +1,26 @@
+using namespace std;
+#include<iostream>
+int main()
+{  
+    /*Here I Try To Create A Basic Calculator For Adding,Substraction,Mutiply
+    and Divide The Two Number*/
+    cout <<"Let's Begin\n";
+	int a,b;
+	//First We Enter The Numbers
+	cout <<"Enter The Value Of A\n";
+	cin >>a;
+	cout <<"Enter The Value Of B\n";
+	cin >>b;
+	//Then Applied the Formula and Wait for Output
+	cout <<"Sum = "  <<a+b;
+	cout <<"\n";
+	cout <<"Remainder = " <<a-b;
+	cout <<"\n";
+	cout <<"Multiplication = " <<a*b;
+	cout <<"\n";
+	cout <<"Division = " <<a/b;
+	cout <<"\n";
+	cout <<"End Of Our Program\n";
+	
+	return 0;
+}
