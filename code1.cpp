a9950e82b32a74f571d8fbd7afc9f6dc2c9768a7
yatharth090 <54454145+yatharth090@users.noreply.github.com>
2020-10-29 11:15:27 +0530

    code1 for the sorting.
    accept it

--- code1.cpp
+++ code1.cpp
@@ -1,67 +1,26 @@
-#include<iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-struct Sample1
-{
-                void getx()
-                {
-                        cin>>x;
-                }
-                int putx()
-                {
-                        return x;
-                }
-                int factorial()
-                {
-                        int prod=1;
-                        for(int i=1; i<=x; i++)
-                                prod*=i;
-                        return prod;
-                }
-                private:
-                int x;
-};
-
-class Sample2
-{
-        int x;
-        public:
-                void getx()
-                {
-                        cin>>x;
-                }
-                int putx()
-                {
-                        return x;
-                }
-                int factorial()
-                {
-                        int prod=1;
-                        for(int i=1; i<=x; i++)
-                                prod*=i;
-                        return prod;
-                }
-};
-
 int main()
 {
-        Sample1 s1;
-        Sample2 s2;
-
-        cout<<"Enter the value of x for s1:";
-        s1.getx();
-
-        cout<<"Enter the value of x for s2:";
-        s2.getx();
-
-        cout<<"\n S1 = "<<s1.putx();
-        cout<<"\nFactorial ="<<s1.factorial();
-        cout<<"\n S2 = "<<s2.putx();
-        cout<<"\nFactorial = "<<s2.factorial();
-
-        return 1;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        if(n%m==0)
+        {
+            if((n/m)%2==0) cout<<"A";
+            else
+            cout<<"B";
+        }
+        else
+        {
+            if((n/m)%2==0) cout<<"B";
+            else
+            cout<<"A";
+        }
+        cout<<"\n";
+    }
+    return 0;
 }
-
-  
-
