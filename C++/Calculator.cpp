bc6b4c95516f43c72ae1373d4ea14566df5df94a
abhirimal <abhirimal009@gmail.com>
2022-10-04 02:25:37 +0545

    add calculator code in c++

---
+++ C++/Calculator.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	
+	int a;
+	int b;
+	
+	cout <<"Enter first number: " << endl;
+	cin >>a;
+	cout <<"Enter second number: " << endl;
+	cin>>b;
+	
+	cout <<"The sum is "<< (a+b)<< endl;
+	cout <<"The difference is "<< (a-b)<< endl;
+	cout <<"The multiple is "<< (a*b)<< endl ;
+	cout <<"The div is "<< (a/b)<< endl;
+	cout <<"The remainder is "<< (a%b);
+
+	
+	
+	return 0;
+}
