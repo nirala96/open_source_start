6a61d1651e75e6e5c0a4ea12d7f45a08e50b80ce
Shivani <shivaniappi3074@gmail.com>
2025-07-20 08:08:28 +0530

    Add Fibonacci

---
+++ Java/FibonacciSequence.java
@@ -0,0 +1,13 @@
+public class FibonacciSequence {
+    public static void main(String[] args) {
+        int n = 10, first = 0, second = 1;
+        System.out.println("Fibonacci Series till " + n + " terms:");
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+
+}
